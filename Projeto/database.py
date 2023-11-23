e7757ac56906cd004e0908f59ef4587b870e85fb
Henrique <hcoelho@ua.pt>
2023-11-23 14:39:27 +0000

    Some Adds and Changes

--- Projeto/database.py
+++ Projeto/database.py
@@ -24,26 +24,6 @@ def serve_img(filename):
 def serve_favicon():
     return send_from_directory('static/img/html', 'icon.ico')
 
-# Function to fetch non-restricted videos for a user
-def get_non_restricted_videos(username):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    # Fetch non-restricted videos for the specified user
-    cursor.execute("SELECT video FROM PTs WHERE user = ? AND restrictedVideo = 0", (username,))
-
-    videos = cursor.fetchall()
-    conn.close()
-    return videos
-
-# Route to display videos for a user
-@app.route('/user_videos/<username>')
-def user_videos(username):
-    videos = get_non_restricted_videos(username)
-    return render_template('videos.html', username=username, videos=videos)
-
-
-
 @app.route('/post')
 def exibir_video():
     video_path = get_video_path_from_database('user1')  # Obtém o caminho do vídeo do banco de dados
@@ -69,6 +49,10 @@ def get_video_path_from_database(username):
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index')
 def blogHome():
     return render_template('index.html')
 
@@ -84,9 +68,6 @@ def contact():
 def post():
     return render_template('post.html')
 
-@app.route('/user_video')
-def user_video():
-    return render_template('user_video.html')
 
 
 if __name__ == '__main__':
