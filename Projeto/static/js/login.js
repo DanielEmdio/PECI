d2bab7f618695c086a97b74bed622adbf81c97b0
TitanHZZ <titanhzz2018@gmail.com>
2023-11-23 22:51:06 +0000

    Login.

--- Projeto/static/js/login.js
+++ Projeto/static/js/login.js
@@ -18,7 +18,6 @@ window.onload = () => {
 function tryLogin () {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    //let errorBox = document.getElementById("errorBox");
 
     if (password === "" || username === "") {
         errorBox.innerHTML = "Por favor preencha todos os campos!";
@@ -32,12 +31,8 @@ function tryLogin () {
                 setCookie("token", info.token); // localStorage.setItem("token", info.token)
                 console.log(info)
                 window.location.pathname = '/'
-            //} else if (info.authentication === "INVALID") {
-                //errorBox.innerHTML = "Usuario ou password invalidos.";
             }
         })
-        .fail(function() {
-            //errorBox.innerHTML = "Ocorreu um erro ao tentar contactar o servidor!";
-        });
+        .fail(function() {});
     }
 }
