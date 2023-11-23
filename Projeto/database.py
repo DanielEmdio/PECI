372e4970e0e015adc1c6911aae013b0ad16058f1
Henrique <hcoelho@ua.pt>
2023-11-23 12:20:54 +0000

    Some Fixs

--- Projeto/database.py
+++ Projeto/database.py
@@ -69,22 +69,18 @@ def get_video_path_from_database(username):
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/index.html')
 def blogHome():
     return render_template('index.html')
 
-@app.route('/about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/contact.html')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/post.html')
+@app.route('/post')
 def post():
     return render_template('post.html')
 
