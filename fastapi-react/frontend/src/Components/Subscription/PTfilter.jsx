8c00b1786b77d4a23e573c8c5517bffb23f9e5ac
DanielEmdio <daniel.emidio@ua.pt>
2024-02-24 16:13:43 +0000

    help me

--- fastapi-react/frontend/src/Components/Subscription/PTfilter.jsx
+++ fastapi-react/frontend/src/Components/Subscription/PTfilter.jsx
@@ -1,6 +1,6 @@
 export default function PTfilter() {
     return (    
-        <div className="navbar bg-base-100 rounded-xl my-10">
+        <div className="flex items-stretch space-between ">
         <select className="select select-ghost w-full max-w-xs">
             <option disabled selected>Pick the best JS framework</option>
             <option>Svelte</option>
@@ -22,26 +22,3 @@ export default function PTfilter() {
     </div>
     )
 }
-
-<ul className="menu lg:menu-horizontal bg-base-200 rounded-box lg:mb-64">
-  <li><a>Item 1</a></li>
-  <li>
-    <details open>
-      <summary>Parent item</summary>
-      <ul>
-        <li><a>Submenu 1</a></li>
-        <li><a>Submenu 2</a></li>
-        <li>
-          <details open>
-            <summary>Parent</summary>
-            <ul>
-              <li><a>item 1</a></li>
-              <li><a>item 2</a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
-    </details>
-  </li>
-  <li><a>Item 3</a></li>
-</ul>
--- fastapi-react/frontend/src/Utils/Router.jsx
+++ fastapi-react/frontend/src/Utils/Router.jsx
@@ -5,19 +5,27 @@ import Subscription from "../Pages/Subscription/Subscription";
 import Progress from "../Pages/Progress/Progress";
 import NewPtTable from "../Pages/Subscription/NewPtTable";
 import SubscriptionDetails from '../Pages/Subscription/SubscriptionDetails';
+import LoginForm from '../Components/LoginForm/LoginForm';
+import RegisterForm from '../Components/RegisterForm/RegisterForm';
+
+
 
 export default function Router() {
 
   const router = createBrowserRouter([
       {
           path: '/', element: <Base/>, children: [
+              
               { path: '/', element: <Home/> },
               { path: '/subscriptions', element: <Subscription/>,},
               { path: '/pt', element: <NewPtTable/>},
               { path: '/progress', element: <Progress/> },
               { path: '/temp', element: <SubscriptionDetails/>}
           ]
-      }
+      },
+      { path: '/login', element: <LoginForm/> },
+      {path: '/register', element: <RegisterForm/> },
+
   ])
   return <RouterProvider router={router} />
   
