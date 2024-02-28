aecd64e14f01dd84b12d980d081d88c5ac83c45b
DanielEmdio <daniel.emidio@ua.pt>
2024-02-28 15:24:26 +0000

    fix mistake

--- fastapi-react/frontend/src/index.js
+++ fastapi-react/frontend/src/index.js
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
-//import * as utils from "./Utils/utils";
+import * as utils from "./Utils/utils";
 import Router from './Utils/Router'
 import React from 'react';
 import './index.css';
 
-//utils.checkAuthentication();
+utils.checkAuthentication();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
