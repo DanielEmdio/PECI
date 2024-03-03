86278062f6d5a7b4c0c5ef95b39efc6d1b54c868
TitanHZZ <titanhzz2018@gmail.com>
2024-03-03 15:08:27 +0000

    Fixed the LogOut.

--- fastapi-react/frontend/src/Components/Sidebar/Sidebar.jsx
+++ fastapi-react/frontend/src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,17 @@
 import { GiGymBag, GiProgression } from "react-icons/gi";
 import { FaUserCircle } from "react-icons/fa";
 import { IoPersonAdd } from "react-icons/io5";
+import * as utils from "../../Utils/utils";
 import { IoMdExit } from "react-icons/io";
 import { FaGear } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 export default function Sidebar() {
+    const handleLogout = () => {
+        utils.eraseCookie("token");
+        utils.goToHome();
+    };
+
     return <div className="drawer-side">
         <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
             <p className="text-4xl font-bold bg-base-200 text-base-content mx-auto">Pocket Coach</p>
@@ -15,7 +21,7 @@ export default function Sidebar() {
             <li className="text-xl font-bold"><Link to={"/progress"}><GiProgression size={32} />Progress</Link></li>
             <li className="text-xl font-bold"><Link to={"/profile"}><FaUserCircle size={32} />Profile</Link></li>
             <li className="text-xl font-bold"><Link to={"/settings"}><FaGear size={32} />Settings</Link></li>
-            <li className="text-xl font-bold"><Link to={"/login"}><IoMdExit size={32} /> Log Out</Link></li>
+            <li className="text-xl font-bold"><button onClick={handleLogout}><IoMdExit size={32} />Log Out</button></li>
         </ul>
     </div>
 }
