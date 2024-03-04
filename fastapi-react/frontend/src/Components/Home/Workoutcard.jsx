c8204a906361454a5b146e0b4f847b6104429115
TitanHZZ <titanhzz2018@gmail.com>
2024-03-04 11:42:21 +0000

    Added an init script for the db on docker.

--- fastapi-react/frontend/src/Components/Home/Workoutcard.jsx
+++ fastapi-react/frontend/src/Components/Home/Workoutcard.jsx
@@ -1,14 +1,13 @@
 import Rating from '@mui/material/Rating';
 import { FaPlay } from "react-icons/fa";
-import Box from '@mui/material/Box';
-import * as React from 'react';
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import { API_URL } from '../../api';
+import { React } from 'react';
 
 export default function Workoutcard({ workout }) {
     /* // Provavelmente deverá ser feito no CategoriesBar.jsx, não é boa ideia fazer aqui....
-    
     const [shownUsernames, setShownUsernames] = React.useState([]);
-
     console.log(workout.PTUsername);
     // Adiciona o username à lista de usernames mostrados
     const addUsernameToShownList = (username) => {
@@ -16,14 +15,12 @@ export default function Workoutcard({ workout }) {
         setShownUsernames(prevUsernames => [...prevUsernames, username]);
         console.log(shownUsernames);
     };
-
     // Verifica se o username já foi mostrado anteriormente
     const isUsernameShown = (username) => {
         console.log('Checando se username já foi mostrado');
         console.log(shownUsernames.includes(username));
         return shownUsernames.includes(username);
     };
-
     // Adiciona o username à lista de usernames mostrados uma única vez após o componente ser montado
     React.useEffect(() => {
         if (workout.PTUsername && !isUsernameShown(workout.PTUsername)) {
@@ -31,17 +28,16 @@ export default function Workoutcard({ workout }) {
             
         }
     }, [workout.PTUsername, shownUsernames]);
-    
     */
-
     // Renderiza o componente
-    console.log(workout.path)
+    // console.log(workout.path)
+
     return (
         <>
             <p>{workout.PTUsername}</p>
 
             <div className="card lg:card-side bg-base-100 shadow-xl">
-                <figure><img src={"http://localhost:8000/images/"+workout.thumbnail} style={{width:"400px", height:"260px"}}alt="workout-image" /></figure>
+                <figure><img src={`${API_URL}/images/${workout.thumbnail}`} style={{ width: "400px", height: "260px" }} alt="workout-image" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{workout.title}</h2>
                     <p>{workout.duration}</p>
