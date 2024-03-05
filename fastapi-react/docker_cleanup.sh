028ea4b107e62791ded0228260a2019d085bcc52
TitanHZZ <titanhzz2018@gmail.com>
2024-03-05 18:51:56 +0000

    Updated the db used in docker.

--- fastapi-react/docker_cleanup.sh
+++ fastapi-react/docker_cleanup.sh
@@ -3,9 +3,8 @@
 set -x
 
 # this will remove all containers, images and volumes present in docker
+echo 'y' | sudo docker system prune -a
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
 sudo docker rmi $(sudo docker images -a -q)
 sudo docker volume rm $(sudo docker volume ls -q)
-
-# sudo docker system prune -a
