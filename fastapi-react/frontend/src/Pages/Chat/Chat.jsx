e18f10cca96c1a36df3c3e1213b9245f3a21ccd8
TitanHZZ <titanhzz2018@gmail.com>
2024-05-12 14:54:19 +0100

    Simple changes.

--- fastapi-react/frontend/src/Pages/Chat/Chat.jsx
+++ fastapi-react/frontend/src/Pages/Chat/Chat.jsx
@@ -19,6 +19,7 @@ export default function Chat() {
                 setRecipient(utils.isNormalUser() ? d.pt.name : d.user.name);
             }
         }).catch((_) => { });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
@@ -50,6 +51,7 @@ export default function Chat() {
         }).catch((_) => {
             startChat();
         });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const startChat = () => {
